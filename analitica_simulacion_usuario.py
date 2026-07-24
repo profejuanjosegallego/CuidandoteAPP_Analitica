9efc2a9d6f277f1b4db8eb7ae263a25403dddfee
CesdeUser <EstudianteCesde@cesde.net>
2026-07-23 20:09:23 -0500

    feat se crea base de simulacion usuario

---
+++ analitica_simulacion_usuario.py
@@ -0,0 +1,11 @@
+id=22
+codigo_ruta=66
+orden_parada= 5685
+direccion = 2546
+barrio = "BuenosAires"
+hora_estimada= 20
+hora_real:20
+minutos_retraso= 20
+tipo_parada= "Escolar"
+estado_parada="Activa"
+category="En marcha"
