721a3aa7c577861cba0a755e81339224c08c7306
Anselmo Ortega <lfkanselmo@gmail.com>
2026-07-23 20:19:58 -0500

    ok

---
+++ analitica_simulacion_conductores.py
@@ -0,0 +1,8 @@
+"documento": "object",
+    "nombre_completo": "object",
+    "licencia_categoria": "category",
+    "telefono": "object",
+    "años_experiencia": "int64",
+    "zona_asignada": "category",
+    "calificacion": "float646",
+    "activo": "bool"
